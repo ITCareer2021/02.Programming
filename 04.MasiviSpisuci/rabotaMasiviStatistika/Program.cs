5229ba92d94c0724ee6d7d6fd8bfb4022158670a
veselin <veselincp2005@gmail.com>
2022-02-05 14:03:20 +0200

    Initial

---
+++ 04.MasiviSpisuci/rabotaMasiviStatistika/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //TODO
+            Console.WriteLine($"min = {numbers.Min()}");
+            Console.WriteLine($"max = {numbers.Max()}");
+            Console.WriteLine($"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Avarage = {numbers.Average()}");
+        }
+    }
+}
