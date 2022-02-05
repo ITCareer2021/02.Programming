976ec5e1e9e1b3d8109cb0454efe1805e28a91b8
Elit0o <eli8.2013@gmail.com>
2022-02-05 13:11:45 +0200

    new files

--- 03.TipoveDanni/realniCommaFlow/Elit0o/Program.cs
+++ 03.TipoveDanni/realniCommaFlow/Elit0o/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ConsoleApp3
+namespace ConsoleApp4
 {
     class Program
     {
@@ -9,7 +9,6 @@ namespace ConsoleApp3
             decimal a = 3.141592653589793238m;
             double b = 1.60217657d;
             decimal c = 7.8184261974584555216535342341m;
-
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
