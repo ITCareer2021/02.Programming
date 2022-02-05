9c9cceebedf7cdc73e0757e68ff949c49f11414a
TsDafov <dafovtsvetomir@gmail.com>
2022-02-05 12:56:57 +0200

    realniCommaFlow

---
+++ 03.TipoveDanni/realniCommaFlow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._Числа_с_плаваща_запетая
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = 3.141592653589793238m;
+            double b = 1.60217657d;
+            decimal c = 7.8184261974584555216535342341m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
