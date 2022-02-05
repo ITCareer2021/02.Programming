0152863b438539ed5db8bae2f5ff73464453f103
TsDafov <dafovtsvetomir@gmail.com>
2022-02-05 14:23:57 +0200

    new files

---
+++ 03.TipoveDanni/textobjectNizObject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._Низове_и_обекти
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = "World";
+
+            object both = $"{hello} {world}";
+
+            string end = (string)both;
+            Console.WriteLine(end);
+        }
+    }
+}
