a7840f558079db0892a97618765257ee727ba15f
Elit0o <eli8.2013@gmail.com>
2022-02-05 11:52:40 +0200

    asansior

---
+++ 03.TipoveDanni/preobrazuvaneAsansior/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            double a = (int)Math.Ceiling((double)n / p);
+            Console.WriteLine(a);
+        }
+    }
+}
