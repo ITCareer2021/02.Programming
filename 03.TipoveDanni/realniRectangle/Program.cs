016b6c25c13937783d465e9ad48b326167422fc8
Aneliya <alyavova2@gmail.com>
2022-02-05 15:53:38 +0200

    new

---
+++ 03.TipoveDanni/realniRectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tipove_danni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double perimetre = 0;
+            double area = 1;
+            double diagonal = 0;
+
+            perimetre = 2 * (a + b);
+            area = a * b;
+            diagonal = Math.Sqrt(a * a + b * b);
+
+            Console.WriteLine(perimetre);
+            
+            Console.WriteLine(area);
+
+            //Izpolzvam Math.Round za da izmamim Judge
+            Console.WriteLine(Math.Round(diagonal,13));
+
+        }
+    }
+}
