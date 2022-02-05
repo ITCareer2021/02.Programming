4d04fd66b1abc1398963152a2925f8894cd51df2
Aneliya <alyavova2@gmail.com>
2022-02-05 23:45:36 +0200

    new files

---
+++ 03.TipoveDanni/celochisleniDecToHexBin/Aneliya05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadachi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string hex = Convert.ToString(n, 16);
+            string bin = Convert.ToString(n, 2);
+
+            
+
+            hex = hex.ToUpper();
+
+            Console.WriteLine(hex);
+            Console.WriteLine(bin);
+        }
+    }
+}
