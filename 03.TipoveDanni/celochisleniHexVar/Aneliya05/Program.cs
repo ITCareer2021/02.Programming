2bbd2de6d52ef126a88a275e5f185ce3a3790a67
Aneliya <alyavova2@gmail.com>
2022-02-05 23:36:13 +0200

    new file

---
+++ 03.TipoveDanni/celochisleniHexVar/Aneliya05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace zadachi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(num, 16));
+        }
+    }
+}
