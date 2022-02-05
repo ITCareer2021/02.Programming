055853d4778f2cd9f27f3bbff3b69b0d0561010e
Aneliya <alyavova2@gmail.com>
2022-02-05 23:20:24 +0200

    new version

---
+++ 03.TipoveDanni/celochisleniCeli/Aneliya05/celochisleni.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zadachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            long c = long.Parse(Console.ReadLine());
+            long d = long.Parse(Console.ReadLine());
+            long e = long.Parse(Console.ReadLine());
+            long f = long.Parse(Console.ReadLine());
+            long g = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+
+
+        }
+    }
+}
