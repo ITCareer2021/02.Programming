3d870b98ab9a6fc5f7823faac21b0081fe304bcf
Kaloyan <kaloianpotter05@gmail.com>
2022-02-05 09:59:54 +0200

    update

---
+++ 02.BroiniSistemi/aritmetikaBin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var z=Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            if (z == "+")
+            {
+                Console.WriteLine(n + b);
+            }
+            if (z == "-")
+            {
+                Console.WriteLine(n - b);
+            }
+            if (z == "/")
+            {
+                Console.WriteLine(n / b);
+            }
+            if (z == "*")
+            {
+                Console.WriteLine(n * b);
+            }
+
+
+
+        }   
+
+            
+
+    }
+
+}
