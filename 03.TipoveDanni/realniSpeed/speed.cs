1063ae26a6fd4105c6dbe9e0283f08f0281565e6
Aneliya <alyavova2@gmail.com>
2022-02-05 16:31:52 +0200

    new

---
+++ 03.TipoveDanni/realniSpeed/speed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TipoveDanni_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distance = float.Parse(Console.ReadLine());
+            float hours = float.Parse(Console.ReadLine());
+            float minutes = float.Parse(Console.ReadLine());
+            float seconds = float.Parse(Console.ReadLine());
+
+            float totalSeconds = hours * 3600 + minutes * 60 + seconds;
+            float totalHours = hours + minutes / 60 + seconds / 3600;
+            float metresInKm = distance / 1000;
+            float metresInMp = distance / 1609;
+
+            float metresPerSecond = distance / totalSeconds;
+            float kilometresPerHour = metresInKm / totalHours;
+            float mpPerHour = metresInMp / totalHours;
+
+            Console.WriteLine(metresPerSecond);
+            Console.WriteLine(kilometresPerHour);
+            Console.WriteLine(mpPerHour);
+        }
+    }
+}
