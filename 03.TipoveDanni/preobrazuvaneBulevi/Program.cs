8b95ec2e790eb08070b7c6a5020b971f44aef95b
TsDafov <dafovtsvetomir@gmail.com>
2022-02-05 13:07:24 +0200

    preobrazuvaneBulevi

---
+++ 03.TipoveDanni/preobrazuvaneBulevi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._Булева_променлива
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
