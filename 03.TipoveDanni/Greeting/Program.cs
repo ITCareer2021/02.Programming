a0a0f57e555cba648f759bfdf42091eec03fa882
Kaloyan <kaloianpotter05@gmail.com>
2022-02-05 20:51:02 +0200

    kaloyan's homework

---
+++ 03.TipoveDanni/Greeting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+
+
+		static void Main(string[] args)
+		{
+			string first = Console.ReadLine();
+			string last = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			Console.WriteLine("Hello, {0} {1}. You are {2} years old.", first, last, age);
+
+
+
+
+		}
+
+	}
+}
---
+++ 03.TipoveDanni/Slujitel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Slujitel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string last = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            var pol = Console.ReadLine();
+            long id = long.Parse(Console.ReadLine());
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine("First Name: {0}",first);
+            Console.WriteLine("Last Name: {0}", last);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Gender: {0} ", pol);
+            Console.WriteLine("Personal ID: {0} ", id);
+            Console.WriteLine("Unique Employee number: {0}",number);
+
+
+        }
+    }
+}
