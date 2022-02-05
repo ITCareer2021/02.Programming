932b60d953c53429f61b86966f8a959990d7b664
Aneliya <alyavova2@gmail.com>
2022-02-05 16:58:13 +0200

    new version

---
+++ 03.TipoveDanni/realniCommaFlow/Aneliya05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
