2b83a176492c7cf2f03bc34e45a49ce5684f5538
Aneliya <alyavova2@gmail.com>
2022-02-05 23:51:34 +0200

    new files

---
+++ 03.TipoveDanni/celochisleniDelenie/Aneliya05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace zadachi_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int c = 0;
+            int totalRemainder = 0;
+
+            while (c < n)
+            {
+                //Console.WriteLine(".");
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                //kum obshtiq ostatuk natrupvame ostatuka na porednata dvoika
+                totalRemainder = totalRemainder + (a % b);
+
+                c++;
+            }
+            Console.WriteLine(totalRemainder);
+        }
+    }
+}
