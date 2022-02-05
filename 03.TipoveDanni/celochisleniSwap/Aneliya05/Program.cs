dab96ca8c18a94449d39c1840caab48a7876058a
Aneliya <alyavova2@gmail.com>
2022-02-05 23:40:47 +0200

    new files

---
+++ 03.TipoveDanni/celochisleniSwap/Aneliya05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadachi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before:");
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+
+            int sw = a;
+            a = b;
+            b = sw;
+
+            Console.WriteLine("After:");
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+        }
+    }
+}
