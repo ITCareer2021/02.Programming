1264bd7bda4421f1c796942c4fa134364e614598
TsDafov <dafovtsvetomir@gmail.com>
2022-02-05 14:36:42 +0200

    new files

---
+++ 03.TipoveDanni/textobjectPiramida/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6._Рефакторирайте_Обем_на_пирамида
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double heigth = double.Parse(Console.ReadLine());
+
+            heigth = (length * width * heigth) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", heigth);
+
+        }
+    }
+}
