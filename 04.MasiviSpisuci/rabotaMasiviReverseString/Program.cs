d535d47aeba761f832d65c2067a5c425e0e62a4d
Elit0o <eli8.2013@gmail.com>
2022-02-06 09:50:42 +0200

    new files

---
+++ 04.MasiviSpisuci/rabotaMasiviReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq; 
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ').ToArray();
+            /*pokazvame elementite v obraten red
+             * for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[arr.Length-i-1]); 
+            }*/
+            Array.Reverse(arr, 0, arr.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
