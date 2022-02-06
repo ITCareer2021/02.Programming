32a8df1db02cbf19022af32f7e93837a4b95f135
Elit0o <eli8.2013@gmail.com>
2022-02-06 14:15:48 +0200

    new files

---
+++ 04.MasiviSpisuci/spisuciObhojdaneChetni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> res = new List<int>();
+            
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    res.Add(nums[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", res));
+        }
+    }
+}
