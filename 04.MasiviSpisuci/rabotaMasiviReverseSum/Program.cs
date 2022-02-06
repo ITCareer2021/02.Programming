837bc56d60579fb9db31282cab5c7d80716f6a69
Elit0o <eli8.2013@gmail.com>
2022-02-06 11:23:01 +0200

    newFiles

---
+++ 04.MasiviSpisuci/rabotaMasiviReverseSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        //ne e vqrno, ako nqkoi umen ima idei...neka pomogne :D
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int r = int.Parse(Console.ReadLine());
+            //natrupvame sumite sled vurtene 
+            int[] sums = new int[arr.Length];
+            for (int i = 1; i <= r; i++)
+            {
+                //broqch na zavurtaniqta
+                for (int c = 0; c < arr.Length; c++)
+                {
+                    //izpolzvame za adresirane na elementite na arr pri zavurtaniq
+                    int position = c;
+                    position = position + i;
+                    if (position >= arr.Length)
+                    {
+                        position = position-arr.Length;
+                    }
+                    //Console.Write("<" + c + " " + position + ">");
+                    sums[c] = sums[c] + arr[position];
+                }
+                //Console.WriteLine();
+            }
+            for (int i = 1; i < sums.Length; i++)
+            {
+                Console.Write(sums[i]+" ");
+            }
+            Console.WriteLine(sums[0]);
+        }
+    }
+}
