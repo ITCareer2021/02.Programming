05d88c83a513c5269d5a74f82eabc847f47c6328
Elit0o <eli8.2013@gmail.com>
2022-02-12 11:09:13 +0200

    new files

---
+++ 04.MasiviSpisuci/sortiraneMasiviSortirane/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int k = i;
+                for (int f = i + 1; f < arr.Length; f++)
+                {
+                    if (arr[f] < arr[k])
+                        k = f;
+                }
+                int swapVar = arr[i];
+                arr[i] = arr[k];
+                arr[k] = swapVar;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
+
