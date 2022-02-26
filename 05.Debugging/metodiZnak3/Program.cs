214b495175908531678e6cae5a47adbe621353fb
Aneliya <alyavova2@gmail.com>
2022-02-26 23:38:16 +0200

    new files

---
+++ 05.Debugging/metodiZnak3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace metodi
+{
+    class Program
+    {
+        static void znak(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", n);
+            }
+            else
+            if (n < 0)
+            {
+                Console.WriteLine("The number {0} is negative.", n);
+            }
+            else
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            znak(int.Parse(Console.ReadLine()));
+        }
+    }
+}
