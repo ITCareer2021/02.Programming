2cf34265942ca9c8ac716095fb1843d1d352f645
Elit0o <eli8.2013@gmail.com>
2022-02-26 09:53:04 +0200

    new files

---
+++ 05.Metodi/Znak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Znak(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", n);
+            }
+            else
+            if (n < 0)
+            {
+                Console.WriteLine("The number {0} is negative.", n);
+            }
+            else 
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Znak(int.Parse(Console.ReadLine()));
+        }
+    }
+}
