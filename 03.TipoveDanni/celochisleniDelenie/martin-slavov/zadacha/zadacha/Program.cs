cdfba55b30a36608f71157d7e36debfe0963b391
Martin Slavov <mstefslav@gmail.com>
2022-02-26 17:03:09 +0200

    newFile

---
+++ 03.TipoveDanni/celochisleniDelenie/martin-slavov/zadacha/zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalRemainder = 0;
+
+            for (int c =0; c < n;c++)
+            {
+                
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                totalRemainder = totalRemainder + (a % b);
+
+            }
+            Console.WriteLine(totalRemainder);
+        }
+    }
+}
