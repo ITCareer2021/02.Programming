900de7b0bc414974d8422a504ac222522e88eeff
Aneliya <alyavova2@gmail.com>
2022-03-02 22:49:28 +0200

    new files

---
+++ 04.MasiviSpisuci/uvodSpisuciVuvejdaneRed/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace spisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for(int i = 0; i<nums.Count; i++)
+            {
+                Console.WriteLine("nums[" + i + "] = " + nums[i]);
+            }
+        }
+    }
+}
