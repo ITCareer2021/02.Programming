230c05c711dde0d82efcb48c1104e57c16a38072
Aneliya <alyavova2@gmail.com>
2022-03-02 23:12:52 +0200

    new files

---
+++ 04.MasiviSpisuci/uvodSpisuciImena/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace spisutsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+            Console.WriteLine(String.Join("; ", names)); 
+        }
+    }
+}
