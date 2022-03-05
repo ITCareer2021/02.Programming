55ce71e97eb2353b151e44c206d8f17274735c23
TsDafov <dafovtsvetomir@gmail.com>
2022-03-05 18:19:29 +0200

    newFiles

---
+++ 04.MasiviSpisuci/rabotaMasiviStatistika/TsDafov/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Статистика_на_масив
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine("Min = {0}", arr.Min());
+            Console.WriteLine("Max = {0}", arr.Max());
+            Console.WriteLine("Sum = {0}", arr.Sum());
+            Console.WriteLine("Average = {0}", arr.Average());
+        }
+    }
+}
