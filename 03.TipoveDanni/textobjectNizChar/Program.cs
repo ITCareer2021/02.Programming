ae4f91117e3c843f08f206530b508b886db200e6
Elit0o <eli8.2013@gmail.com>
2022-03-05 17:43:01 +0200

    new files

---
+++ 03.TipoveDanni/textobjectNizChar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a = Console.ReadLine();
+			string b = Console.ReadLine();
+			string b1 = Console.ReadLine();
+			string b2 = Console.ReadLine();
+			string a1 = Console.ReadLine();
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(a1);
+		}
+	}
+}
