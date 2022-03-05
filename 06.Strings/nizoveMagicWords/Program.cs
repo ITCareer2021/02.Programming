2d454428b35f31916e1099738ec8f99c3585c087
Elit0o <eli8.2013@gmail.com>
2022-03-05 11:14:18 +0200

    new files

---
+++ 06.Strings/nizoveMagicWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+            char[] word1 = words[0].ToCharArray();
+            char[] word2 = words[1].ToCharArray();
+
+            var w1 = word1.ToList();
+            var w2 = word2.ToList();
+
+            var w1nd = w1.Distinct().ToList();
+            var w2nd = w2.Distinct().ToList();
+
+            if (w1nd.Count == w2nd.Count)
+            {
+                Console.WriteLine("true");
+            }
+
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
