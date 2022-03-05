de76b8dd2bc5505c1f4857165a68539ba2eb8715
Elit0o <eli8.2013@gmail.com>
2022-03-05 11:10:11 +0200

    new files

---
+++ 08. Dictionaries/dictionaryOdd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            string[] words = input.Split(' ');
+            var counts = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                    counts[word] = 1; //dobavqme dumata v rechnika, 1va sreshta
+            }
+
+            var results = new List<string>();
+            foreach (var pair in counts)
+            {
+                if (pair.Value % 2 == 1)
+                {
+                    results.Add(pair.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", results));
+
+        }
+    }
+}
---
+++ 08. Dictionaries/dictionaryPhonebook/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            var tels = new Dictionary<string, string>();
+            var namesToSearch = new List<string>();
+
+            while (!exit)
+            {
+                string[] input = Console.ReadLine().Split(' ').ToArray();
+
+                if (input[0] == "END")
+                {
+                    exit = true;
+                }
+                else
+                if (input[0] == "A")
+                {
+                    tels[input[1]] = input[2];
+                }
+                else //Search input[0] = "S"
+                {
+                    namesToSearch.Add(input[1]);
+
+                    //foreach (var oneName in namesToSearch)
+                    var oneName = input[1];
+                    {
+                        if (tels.ContainsKey(oneName))
+                        {
+                            tels.TryGetValue(oneName, out var value);
+                            Console.WriteLine("{0} -> {1}", oneName, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Contact {0} does not exist.", oneName);
+                        }
+                    }
+                }    
+            }
+        }
+    }
+}
