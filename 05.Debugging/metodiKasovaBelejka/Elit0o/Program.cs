b6f64220480990adf0220171b97e5a48158136cf
Elit0o <eli8.2013@gmail.com>
2022-03-09 22:08:37 +0200

    new files

---
+++ 05.Debugging/metodiKasovaBelejka/Elit0o/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TopRow();
+            MiddleRow();
+            BottomRow();
+
+        }
+        static void TopRow()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        static void MiddleRow()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void BottomRow()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 BG");
+        }
+    }
+}
---
+++ 05.Debugging/metodiOtpechatvaneNaTriagalnik/Elit0o/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintTriangle(int.Parse(Console.ReadLine()));
+        }
+        static void PrintTriangle(int n)
+        {
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(k + " ");
+                }
+
+                Console.Write("\n");
+            }
+            for (int i = n; i > 0; i--)
+            {
+                for (int k = 1; k <= i - 1; k++)
+                {
+                    Console.Write(k + " ");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
