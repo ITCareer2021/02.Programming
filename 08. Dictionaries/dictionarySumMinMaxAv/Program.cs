b056b46edf9574acaba92bd895ca19ca0244347d
Elit0o <eli8.2013@gmail.com>
2022-03-09 21:30:55 +0200

    new files

---
+++ 08. Dictionaries/dictionarySumMinMaxAv/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            //var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList(); не е за judge
+            //var first = numbers.Take(1).ToList(); не е за judge
+            //var last = numbers.Skip(numbers.Count-1).Take(1).ToList(); не е за judge
+
+            //Console.WriteLine($"First = {first[0]}"); не е за judge
+            Console.WriteLine($"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Min = {numbers.Min()}");
+            Console.WriteLine($"Max = {numbers.Max()}");
+            Console.WriteLine($"Average = {numbers.Average()}");
+            //Console.WriteLine($"Last = {last[0]}"); не е за judge
+        }
+    }
+}
