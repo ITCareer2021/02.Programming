7ddb79b6ae2b65bd4fe662846984f34f9d2e2bcf
Aneliya <alyavova2@gmail.com>
2022-03-12 15:02:25 +0200

    new files

---
+++ 07.MnogomerniMasiv/mnogomerniVhodIzhod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mnogomerni_masivi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine()); //въвеждаме брой редове
+            int cols = int.Parse(Console.ReadLine()); //въвеждаме брой колони
+            int[,] matrix = new int[rows, cols]; //заделяме съответния брой елементи
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    //Console.Write("matrix[{0},{1}] = ", row, col);
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("{0} ", matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
