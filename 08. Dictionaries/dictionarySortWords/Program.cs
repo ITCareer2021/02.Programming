0a5e3afb3b4f93dcecd20f34896e3871446f6e25
Elit0o <eli8.2013@gmail.com>
2022-03-13 09:17:11 +0200

    new files

---
+++ 08. Dictionaries/dictionarySortWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Сортиране_на_кратки_думи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '\\', '\"', '\'', '/', '!', '?', ' ' };
+            string sentence = Console.ReadLine().ToLower();
+            string[] words = sentence.Split(separators);
+            var result = words.Where(w => w != "").Where(duma => duma.Length < 5)
+              // TODO: филтирайте по дължина < 5
+              .OrderBy(w => w).Distinct();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
