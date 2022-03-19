9a2db7b4129592c7e7301227b90e5c25ebdb1de6
Aneliya <alyavova2@gmail.com>
2022-03-19 14:47:49 +0200

    another version

---
+++ 03.TipoveDanni/preobrazuvaneAsansior/Aneliya/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tipoveDanni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInElevator = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int pulenKurs = peopleInElevator / capacity;
+            if(peopleInElevator % capacity != 0)
+            {
+                pulenKurs = pulenKurs + 1;
+            }
+            Console.WriteLine(pulenKurs);
+        }
+    }
+}
