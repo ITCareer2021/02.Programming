90f40d4c5a559e3da84e95d7b053377e579a1de2
Aneliya <alyavova2@gmail.com>
2022-03-19 15:40:27 +0200

    another version

---
+++ 03.TipoveDanni/preobrazuvaneBulevi/Aneliya05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tipoveDanni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine(); //variant bez preobrazuvane v bulev tip
+            if(n == "True")
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            if (n == "False")
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
