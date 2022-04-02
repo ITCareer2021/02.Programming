51a5ed0ff343d4dda39b41ea4d1b54810d9f6795
Aneliya <alyavova2@gmail.com>
2022-04-02 13:33:41 +0300

    new files

---
+++ 03.TipoveDanni/realniCircle/Aneliya05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TipoveDanni_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double s = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("{0:f12}", s);
+        }
+    }
+}
