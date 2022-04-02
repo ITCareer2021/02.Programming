07859bfa2b4bbf1a3b7e7a131230f4d43d5ef843
Aneliya <alyavova2@gmail.com>
2022-04-02 14:28:30 +0300

    another version

---
+++ 03.TipoveDanni/textobjectGreeting/Aneliya05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace New_Tipove_Danni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello, {firstName} {lastName}. You are {age} years old.");
+        }
+    }
+}
