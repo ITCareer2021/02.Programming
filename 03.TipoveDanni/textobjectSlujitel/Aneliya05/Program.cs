58121435d7105091492bbc3fe0758258bdb05ea9
Aneliya <alyavova2@gmail.com>
2022-04-03 14:10:01 +0300

    new version

---
+++ 03.TipoveDanni/textobjectSlujitel/Aneliya05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace typesOfData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            char gender = Convert.ToChar(Console.ReadLine());
+            ulong id = ulong.Parse(Console.ReadLine());
+            int employeeNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"First name: {firstName} \n Last name: {lastName} \n Age: {age} \n Gender: {gender} \n Personal ID: {id} \n Unique Employee number: {employeeNumber}");
+        }
+    }
+}
