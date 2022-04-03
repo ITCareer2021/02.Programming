21211dd971d48848fe7a94f9059ad9a3c4ccd079
Aneliya <alyavova2@gmail.com>
2022-04-03 13:52:50 +0300

    new version

---
+++ 03.TipoveDanni/textobjectObrushtane/Aneliya05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace typesOfData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine()); //1-ви вариант за директно прочитане на char ог конзолата
+            char b = Convert.ToChar(Console.ReadLine()); 
+            char c = Console.ReadLine()[0]; //2-ри вариант за директно прочитане на char ог конзолата
+            Console.WriteLine("{0}{1}{2}", c,b,a);
+        }
+    }
+}
