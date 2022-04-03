8e453016e018ef07b6366cdbc6fc47f5b2ca3a2f
Aneliya <alyavova2@gmail.com>
2022-04-03 13:10:42 +0300

    new

---
+++ 03.TipoveDanni/textobjectNizChar/Aneliya05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace New_Tipove_Danni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string one = Console.ReadLine();
+            string two = Console.ReadLine();
+            string three = Console.ReadLine();
+            string second = Console.ReadLine();
+            Console.WriteLine(first);
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+            Console.WriteLine(three);
+            Console.WriteLine(second);
+        }
+    }
+}
