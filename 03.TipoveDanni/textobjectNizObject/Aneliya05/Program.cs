605bcaed92e70afd70be1014575ca1fc1884a1d2
Aneliya <alyavova2@gmail.com>
2022-04-03 13:21:52 +0300

    new

---
+++ 03.TipoveDanni/textobjectNizObject/Aneliya05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace New_Tipove_Danni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello";
+            string b = "World";
+            object ab = a + " " + b;
+            string c = (string) ab;
+            Console.WriteLine(c);    
+        }
+    }
+}
