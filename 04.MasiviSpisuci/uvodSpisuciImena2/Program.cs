ae2f7ae65a7baf3b9c2797c478eb77935bb4a8d0
Aneliya <alyavova2@gmail.com>
2022-04-08 00:27:18 +0300

    new

---
+++ 04.MasiviSpisuci/uvodSpisuciImena2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace list_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(',').ToList();
+            foreach(var name in names)
+            {
+                Console.WriteLine(String.Join(" ", name.Split(' ').Reverse()));
+            }
+            
+        }
+    }
+}
