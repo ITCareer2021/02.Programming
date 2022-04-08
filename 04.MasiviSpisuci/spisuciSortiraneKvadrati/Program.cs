5223de836bd32d0987aeff1f299e5d3112401dfe
Aneliya <alyavova2@gmail.com>
2022-04-08 23:23:26 +0300

    new

---
+++ 04.MasiviSpisuci/spisuciSortiraneKvadrati/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace list_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if(Math.Sqrt(numbers[i]) == (int) Math.Sqrt(numbers[i]))
+                {
+                    result.Add(numbers[i]);
+                    result.Sort();
+                    result.Reverse();
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
