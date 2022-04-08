232376c823998a3247131e8d829373acbf2ba54e
Aneliya <alyavova2@gmail.com>
2022-04-08 13:04:59 +0300

    new

---
+++ 04.MasiviSpisuci/spisuciObhojdaneChetni/Aneliya05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace list_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            foreach(var number in numbers)
+            {
+                if(number%2==0)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+            
+        }
+    }
+}
